401f04faa454cab67d9a8e9e066907a4fb40f549
Pedro Parmeggiani <parmeggianipedro@gmail.com>
2019-09-16 17:52:04 -0300

    Last push of the day

--- models/product.js
+++ models/product.js
@@ -1,22 +1,7 @@
-const fs = require('fs');
-const path = require('path');
+const db = require('../util/database')
+
 const Cart = require('./cart')
 
-const p = path.join(
-  path.dirname(process.mainModule.filename),
-  'data',
-  'products.json'
-);
-
-const getProductsFromFile = cb => {
-  fs.readFile(p, (err, fileContent) => {
-    if (err) {
-      cb([]);
-    } else {
-      cb(JSON.parse(fileContent));
-    }
-  });
-};
 
 module.exports = class Product {
   constructor(id, title, imageUrl, description, price) {
@@ -28,44 +13,15 @@ module.exports = class Product {
   }
 
   save() {    
-    getProductsFromFile(products => {
-      if (this.id) {
-        let existingProductIndex = products.findIndex(prod => prod.id === this.id)
-        let updatedProducts = [...products]
-        updatedProducts[existingProductIndex] = this
-        fs.writeFile(p, JSON.stringify(updatedProducts), err => {
-          console.log(err);
-        });
-      } else {
-        this.id = Math.random().toString();      
-        products.push(this);
-        fs.writeFile(p, JSON.stringify(products), err => {
-          console.log(err);
-        });
-      }
-    });
   }
   
   static deleteById(id) {
-    getProductsFromFile(products => {
-      let product = products.find(prod => prod.id === id)
-      let updatedProducts = products.filter(p => p.id !== id) //this returns all elements as part of new array that matches predicate      
-      fs.writeFile(p, JSON.stringify(updatedProducts), err => {        
-        if (!err) {
-          Cart.deleteProduct(id, product.price)
-        }
-      });      
-    }) 
   }
 
-  static fetchAll(cb) {
-    getProductsFromFile(cb);
+  static fetchAll() {
+    return db.execute('SELECT * FROM products')    
   }
 
   static findById(id, cb) {
-    getProductsFromFile(products => {
-      let product = products.find(p => p.id === id)
-      cb(product)
-    }) 
   }
-};
+}
